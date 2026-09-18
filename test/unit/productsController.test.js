687489fe49a2d8f36cc02aa3979dcc7c619123af
Daniel Carlos de Araujo <111451024+Danielcbaraujo@users.noreply.github.com>
2026-09-18 12:20:50 +0000

    add testes unitarios findall

--- test/unit/productsController.test.js
+++ test/unit/productsController.test.js
@@ -3,6 +3,10 @@ const productsService = require("../../src/services/productsService");
 
 jest.mock("../../src/services/productsService");
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 test("deve retornar 400 quando minPrice for maior que maxPrice", async () => {
   const req = {
     query: {
@@ -26,6 +30,134 @@ test("deve retornar 400 quando minPrice for maior que maxPrice", async () => {
 
   expect(productsService.findAll).not.toHaveBeenCalled();
 });
+
+test("deve retornar todos os produtos quando nenhum filtro for informado", async () => {
+  const products = [
+    {
+      id: 1,
+      name: "Mouse",
+      price: "80.00",
+    },
+    {
+      id: 2,
+      name: "Teclado",
+      price: "150.00",
+    },
+  ];
+
+  productsService.findAll.mockResolvedValue(products);
+
+  const req = {
+    query: {},
+  };
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  await productsController.findAll(req, res);
+
+  expect(productsService.findAll).toHaveBeenCalledWith();
+  expect(res.status).toHaveBeenCalledWith(200);
+  expect(res.json).toHaveBeenCalledWith(products);
+});
+
+test("deve retornar produtos quando apenas minPrice for informado", async () => {
+  const products = [
+    {
+      id: 2,
+      name: "Teclado",
+      price: "150.00",
+    },
+    {
+      id: 3,
+      name: "Monitor",
+      price: "500.00",
+    },
+  ];
+
+  productsService.findAll.mockResolvedValue(products);
+
+  const req = {
+    query: {
+      minPrice: "100",
+    },
+  };
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  await productsController.findAll(req, res);
+
+  expect(productsService.findAll).toHaveBeenCalledWith(100, undefined);
+
+  expect(res.status).toHaveBeenCalledWith(200);
+
+  expect(res.json).toHaveBeenCalledWith(products);
+});
+
+test("deve retornar produtos quando apenas maxPrice for informado", async () => {
+  const products = [
+    {
+      id: 1,
+      name: "Mouse",
+      price: "80.00",
+    },
+    {
+      id: 2,
+      name: "Teclado",
+      price: "150.00",
+    },
+  ];
+
+  productsService.findAll.mockResolvedValue(products);
+
+  const req = {
+    query: {
+      maxPrice: "200",
+    },
+  };
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  await productsController.findAll(req, res);
+
+  expect(productsService.findAll).toHaveBeenCalledWith(undefined, 200);
+
+  expect(res.status).toHaveBeenCalledWith(200);
+  expect(res.json).toHaveBeenCalledWith(products);
+});
+
+
+test("deve retornar 400 quando maxPrice estiver vazio", async () => {
+  const req = {
+    query: {
+      maxPrice: "",
+    },
+  };
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  await productsController.findAll(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(400);
+
+  expect(res.json).toHaveBeenCalledWith({
+    error: "maxPrice deve ser um número",
+  });
+
+  expect(productsService.findAll).not.toHaveBeenCalled();
+});
+
 
 //tests findById
 
@@ -51,7 +183,6 @@ test("deve retornar 400 quando o id for inválido", async () => {
 
   expect(productsService.findById).not.toHaveBeenCalled();
 });
-
 
 test("deve retornar 404 quando não encontrar um produto", async () => {
   productsService.findById.mockResolvedValue([]);
