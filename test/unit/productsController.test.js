30f70d6e78483894a2c177b9b58660349ba034e3
Daniel Carlos de Araujo <111451024+Danielcbaraujo@users.noreply.github.com>
2026-09-22 20:44:03 +0000

    add testes do create

--- test/unit/productsController.test.js
+++ test/unit/productsController.test.js
@@ -331,6 +331,47 @@ test("deve retornar 400 quando maxPrice não for um número", async () => {
   expect(productsService.findAll).not.toHaveBeenCalled();
 });
 
+test("deve retornar 500 quando ocorrer um erro ao buscar produtos", async () => {
+  const req = {
+    query: {
+      maxPrice: "100",
+    },
+  };
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  productsService.findAll.mockRejectedValue(new Error("Erro no banco"));
+
+  await productsController.findAll(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(500);
+
+  expect(res.json).toHaveBeenCalledWith({
+    error: "Erro interno do servidor",
+  });
+});
+
+test("deve retornar 500 quando ocorrer um erro ao buscar todos os produtos", async () => {
+  const req = {
+    query: {},
+  };
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  productsService.findAll.mockRejectedValue(new Error("Erro no banco"));
+  await productsController.findAll(req, res);
+  expect(res.status).toHaveBeenCalledWith(500);
+
+  expect(res.json).toHaveBeenCalledWith({
+    error: "Erro interno do servidor",
+  });
+});
+
 //tests findById
 
 test("deve retornar 400 quando o id for inválido", async () => {
@@ -410,4 +451,115 @@ test("deve retornar 200 quando encontrar um produto", async () => {
   expect(res.status).toHaveBeenCalledWith(200);
 
   expect(res.json).toHaveBeenCalledWith(product);
+});
+
+test("deve retornar 500 quando ocorrer um erro ao buscar produto por id", async () => {
+  const req = {
+    params: {
+      id: "1",
+    },
+  };
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  productsService.findById.mockRejectedValue(new Error("Erro no banco"));
+
+  await productsController.findById(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(500);
+
+  expect(res.json).toHaveBeenCalledWith({
+    error: "Erro interno do servidor",
+  });
+});
+
+//test create
+
+test("deve retornar 400 quando price não for válido", async () => {
+  const req = {
+    body: {
+      name: "Mouse",
+      price: "abc",
+    },
+  };
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  await productsController.create(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(400);
+
+  expect(res.json).toHaveBeenCalledWith({
+    error: "price deve ser um número",
+  });
+
+  expect(productsService.create).not.toHaveBeenCalled();
+});
+
+test("deve retornar 201 quando produto for criado", async () => {
+  const req = {
+    body: {
+      name: "Mouse",
+      price: "80",
+    },
+  };
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  productsService.create.mockResolvedValue([
+    {
+      id: 1,
+      name: "Mouse",
+      price: 80,
+    },
+  ]);
+
+  await productsController.create(req, res);
+
+  expect(productsService.create).toHaveBeenCalledWith("Mouse", 80);
+
+  expect(res.status).toHaveBeenCalledWith(201);
+
+  expect(res.json).toHaveBeenCalledWith([
+    {
+      id: 1,
+      name: "Mouse",
+      price: 80,
+    },
+  ]);
+});
+
+test("deve retornar 400 quando o service lançar erro", async () => {
+  const req = {
+    body: {
+      name: "",
+      price: "80",
+    },
+  };
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  productsService.create.mockRejectedValue(
+    new Error("Nome do produto é obrigatório"),
+  );
+
+  await productsController.create(req, res);
+
+  expect(res.status).toHaveBeenCalledWith(400);
+
+  expect(res.json).toHaveBeenCalledWith({
+    error: "Nome do produto é obrigatório",
+  });
 });
